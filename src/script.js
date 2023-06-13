8b9491207fd748ff386828e960ec21689b6aa311
Tri <trimpham98@gmail.com>
2023-06-12 23:27:38 -0700

    Change spotLight map size width and height to 1024

--- src/script.js
+++ src/script.js
@@ -54,6 +54,8 @@ scene.add(directionalLightCameraHelper);
 const spotLight = new THREE.SpotLight(0xffffff, 0.4, 10, Math.PI * 0.3);
 
 spotLight.castShadow = true;
+spotLight.shadow.mapSize.width = 1024;
+spotLight.shadow.mapSize.height= 1024;
 
 spotLight.position.set(0, 2, 2);
 
