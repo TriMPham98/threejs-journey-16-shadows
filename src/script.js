3d99016c0e2689020625de9903077b70e1c41fe0
Tri <trimpham98@gmail.com>
2023-06-12 23:08:14 -0700

    Add PCF soft shadow map

--- src/script.js
+++ src/script.js
@@ -44,7 +44,7 @@ directionalLight.shadow.camera.bottom = -2;
 directionalLight.shadow.camera.left = -2;
 directionalLight.shadow.camera.near = 1;
 directionalLight.shadow.camera.far = 6;
-directionalLight.shadow.radius = 10;
+// directionalLight.shadow.radius = 10;
 
 const directionalLightCameraHelper = new THREE.CameraHelper(directionalLight.shadow.camera);
 directionalLightCameraHelper.visible = false;
@@ -124,6 +124,7 @@ renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
 /**
  * Animate
