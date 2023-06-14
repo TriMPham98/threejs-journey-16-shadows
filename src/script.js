5c2220a50cd535ee40745c574b16677a2f51b3aa
Tri <trimpham98@gmail.com>
2023-06-13 23:31:32 -0700

    Add point light mapSize width and height 1024, near and far range 0.1 to 5

--- src/script.js
+++ src/script.js
@@ -73,6 +73,10 @@ scene.add(spotLightCameraHelper);
 const pointLight = new THREE.PointLight(0xffffff, 0.3);
 
 pointLight.castShadow = true;
+pointLight.shadow.mapSize.width = 1024;
+pointLight.shadow.mapSize.height = 1024;
+pointLight.shadow.camera.near = 0.1;
+pointLight.shadow.camera.far = 5;
 
 pointLight.position.set(-1, 1, 0);
 
